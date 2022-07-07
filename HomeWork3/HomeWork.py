c7df1738ceffe353c55c5a6ec2eb1bef92f86558
Godzik82 <godzik82@mail.ru>
2022-07-07 22:18:22 +0400

    Add homework3

---
+++ HomeWork3/HomeWork.py
@@ -0,0 +1,11 @@
+from task1_def import task1
+from task2_def import task2
+from task3_def import task3
+
+def main():
+    task1()
+    task2()
+    task3()
+
+if __name__ == '__main__':
+    main()
