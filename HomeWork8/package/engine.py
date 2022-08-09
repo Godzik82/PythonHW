2e2a8af10695a422528dea3178648e7733920298
Godzik82 <godzik82@mail.ru>
2022-08-09 22:22:49 +0400

    add homework8

---
+++ HomeWork8/package/engine.py
@@ -0,0 +1,47 @@
+import curses
+import random
+import time
+from tkinter import LEFT
+from snake import Snake
+from food import Food
+from table import Field
+
+def game():
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+    field = Field()
+    snake = Snake()
+    food = Food(random.randint(0, 14), random.randint(0, 14), random.choice(['G', 'D', 'E']))
+    head_x = snake.body[0][0]
+    head_y = snake.body[0][1]
+
+    while 0 <= head_x < 15 and 0 <= head_y < 15:
+        
+        stdscr.clear()             
+        field.turn(snake, food)
+        for elm in field.pole:
+            a = ' '.join(elm) + '\n'
+            stdscr.addstr(a)
+            stdscr.refresh()
+
+        key = stdscr.getch()
+
+        if key == 452:
+            head_y -= 1
+        elif key == 454:
+            head_y += 1
+        elif key == 450:
+            head_x -= 1
+        elif key == 456:
+            head_x += 1
+
+        snake.move(head_x, head_y)
+
+        if head_x == food.x and head_y == food.y:
+            snake.eat()
+            food = Food(random.randint(0, 14), random.randint(0, 14), random.choice(['G', 'D', 'E']))
+    
+    print('Game Over')
+
---
+++ HomeWork8/package/food.py
@@ -0,0 +1,8 @@
+class Food():
+
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+
+    
